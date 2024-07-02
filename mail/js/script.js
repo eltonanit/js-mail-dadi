8a47ae354ec5dce690956bf38aa9d3430ad21a84
eltonanit <elton.ani@gmail.com>
2024-07-02 17:24:11 +0200

    [UPD] Recupero Dati dal DOM e aggiungo pulsanti

---
+++ mail/js/script.js
@@ -0,0 +1,23 @@
+// EMAIL ARRAY
+
+//definisco Array
+
+let emails = ['red@mail.com', 'black@mail.com', 'green@mail.com', 'blue@mail.com'];
+
+//Recupero pulsante dal DOM
+
+const btn = document.getElementById('check');
+
+//Aggiungo evento clcik al pulsante
+btn.addEventListener('click', function() {
+
+    //recupero i valori input da email
+    let user_email = document.getElementById('email').value;
+
+    //Definico una variabile  flag per il controllo della presenza della mail
+    let check_email= false;
+
+    //Ciclo Array
+}
+
+)
